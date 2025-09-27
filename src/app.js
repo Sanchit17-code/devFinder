05cb2bb8a8ab78f6a5dfcd126521e3d113998190
Sanchit Agrawal <sanchitagrawal429@gmail.com>
2025-09-27 20:06:33 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("hello this is the test route");
-})
+app.use(
+    "/user",
+    (req,res,next)=>{
+        console.log("RH1");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("RH2");
+        res.send("last route handler")
+        next();
+    },
+)
+
+// app.get("/test",(req,res)=>{
+//     res.send("hello this is the test route");
+// })
+// app.get("/test/random",(req,res)=>{
+//     console.log(req.query);
+//     res.send("hello this is the test and random route");
+// })
+// app.get("/user/:userId",(req,res)=>{
+//     console.log(req.params);
+//     res.send("hello this is the user with user id");
+// })
 
 app.listen(7777,()=>{
     console.log("listening to the port 7777")
