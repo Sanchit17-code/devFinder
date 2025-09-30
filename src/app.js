b2c6006bb910923ab523742d2ccb11b42414feb0
Sanchit Agrawal <sanchitagrawal429@gmail.com>
2025-09-30 21:23:47 +0530

    filter by email and feed API

--- src/app.js
+++ src/app.js
@@ -6,6 +6,39 @@ const User = require("./models/user")
 
 app.use(express.json());
 
+app.get("/user", async (req,res)=>{
+    const emailId = req.body.emailId;
+    console.log("emilID",emailId);
+    try{
+        const users = await User.find({emailId: emailId});
+        if(users.length>0){
+            res.send(users)
+        }
+        else{
+            res.status(404).send("No Users Found")
+        }
+    }
+    catch(err){
+        res.status.send(400).send("Something went wrong");
+    }
+
+})
+
+app.get("/feed", async (req,res)=>{
+    try{
+        const users = await User.find({})
+        if(users.length>0){
+            res.send(users);
+        }
+        else{
+            res.status(404).send("No users found")
+        }
+    }
+    catch(err){
+        res.status(400).send("Somehting went wrong");
+    }
+})
+
 app.post("/signup", async (req,res)=>{
     console.log("req.body",req.body);
     const user = new User(req.body)
