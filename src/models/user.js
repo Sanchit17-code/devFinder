7bc7ecaf7b2cb3d100aabd809de93cd08a525d9d
Sanchit Agrawal <sanchitagrawal429@gmail.com>
2025-10-03 22:02:36 +0530

    some small validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -16,10 +17,20 @@ const userSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
         trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address: " + value );
+            }
+        }
     },
     password:{
         type: String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong psssword " + value );
+            }
+        }
     },
     age:{
         type: Number,
@@ -35,6 +46,11 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl :{
         type: String,
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid URL: " + value );
+            }
+        }
     },
     about:{
         type: String,
