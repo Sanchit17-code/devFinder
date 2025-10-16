32cc91676721089ed85f80d88d955e9e9257e8d9
Sanchit Agrawal <sanchitagrawal429@gmail.com>
2025-10-16 20:10:45 +0530

    adding patch API for profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,13 +2,32 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 
-profileRouter.get("/profile", userAuth, async (req,res)=>{
+profileRouter.get("/profile/view", userAuth, async (req,res)=>{
     try{
         const userProfile = req.user;
         res.send(userProfile);
     }
     catch(e){
         res.status(400).send("Error: "+ e.message);
+    }
+})
+
+profileRouter.patch("/profile/edit", userAuth, async(req,res)=>{
+    try{
+        const loggedInUser = req.user;
+         console.log("loggedIn user",loggedInUser);
+        Object.keys(req.body).forEach(eachField=>{
+            loggedInUser[eachField] = req.body[eachField];
+        })
+        await loggedInUser.save();
+        console.log("loggedIn user",loggedInUser);
+        res.json({
+            message: "Data has been updated",
+            data: loggedInUser
+        })
+    }
+    catch(e){
+        res.status(400).send("ERROR: "+ e.message);
     }
 })
 
